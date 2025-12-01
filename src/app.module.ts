6bd8f702842c979b83c20916e3fb94a49792c8bc
mamun2519 <juborajislam46@gmail.com>
2025-12-01 17:03:58 +0600

    feat: added to mongose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL as string),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
