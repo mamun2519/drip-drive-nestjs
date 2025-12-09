5f00e5919bd4f01a3a6b7071f0444bd4281155fb
mamun2519 <juborajislam46@gmail.com>
2025-12-09 21:55:39 +0600

    feat: added to user interface

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,14 +12,15 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import type { Request } from 'express';
+import { IUser } from './user.interface';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserData);
+  create(@Body() createUserDto: IUser) {
+    return this.userService.create(createUserDto);
   }
 
   @Get()
